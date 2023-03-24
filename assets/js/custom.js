bc47a5178fecff7a1d881831cab7719918f43a20
Daniyudha <dani.yudha33@gmail.com>
2023-03-25 02:25:23 +0700

    minus responsive

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -49,6 +49,22 @@
         nextArrow: '<button class="NextArrow"></button>',
     });
 
+    $(".other-Slider").slick({
+        autoplay: true,
+        autoplaySpeed: 10000,
+        speed: 600,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        pauseOnHover: false,
+        dots: true,
+        pauseOnDotsHover: true,
+        cssEase: 'linear',
+        // fade:true,
+        draggable: false,
+        prevArrow: '<button class="PrevArrow"></button>',
+        nextArrow: '<button class="NextArrow"></button>',
+    });
+
     $('.search-icon a').on("click", function(event) {
         event.preventDefault();
         $("#search").addClass("open");
